d2dbbbe36d1d2b6c52c3ad57c252c2089e0302a4
Sivasaaniya <sssaaniya3@gmail.com>
2026-04-26 23:21:35 +0530

    c

--- Array/Storearr.java
+++ Array/Storearr.java
@@ -1,8 +1,7 @@
 public class Storearr {
 public static void main(String[] args) {
-int[] arr = {1, 2, 3, 4, 5};  // storing 5 values
-
-for (int num : arr) {
-System.out.print(num + " ");  // printing values
+int[] arr = {1, 2, 3, 4, 5}; 
+for (int num:arr) {
+System.out.print(num + " ");
 }}
 }
